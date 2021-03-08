05edfeea3e766905e1118328c421bf4861cff081
ItZzMJ <jannik.moebius@gmail.com>
2021-03-08 14:06:34 +0100

    main.c cleanup + Readme update

--- README.md
+++ README.md
@@ -76,20 +76,27 @@ bei negativem Vorzeichen -1 und bei 0 wird 0 zurückgegeben.
 int main()
 {
     int daysOfMonth, dayOfYear;
+    
+    //Leeres Datum erzeugen
     Date date;
 
     printf("**** Test der neuen Funktionen ****\n");
 
+    //Datum über CLI einlesen
     input_date(&date);
 
+    //Überprüfen ob das Datum existiert
     if(exists_date(date)) {
         printf("Das Datum %d.%d.%d existiert.\n", date.day, date.month, date.year);
     } else  {
         printf("Das Datum %d.%d.%d existiert nicht.\n", date.day, date.month, date.year);
     }
 
+    //Die Tage des Monats herausfinden
     daysOfMonth = get_days_for_month(date.month, date.year);
     printf("Der %d. Monat hat %d Tage.\n", date.month, daysOfMonth);
+    
+    //Den Tag des Jahres herausfinden
     dayOfYear = day_of_the_year(date);
     printf("Der %d.%d.%d ist der %d. Tag des Jahres.\n", date.day, date.month, date.year, dayOfYear);
 
--- main.c
+++ main.c
@@ -9,103 +9,32 @@
 #include <stdlib.h>
 
 /**
- * Formatierte Ausgabe zum Testen der vorherigen Funktionen
- *
- * @param x
- * @param y
- * @param erg
+ * Testfunktion zum Testen der Bibliothek timelib.c
  */
-void ausgabe(int x, int y, int erg)
-{
-    if(y == NULL) printf(" %d\t", x);
-    else printf(" %d, %d ", x,y);
-
-    printf("\t| %d \n", erg);
-}
-
-/**
- * Testfunktion für Min-, Max-, und Vorzeichen-funktion
- * @return
- */
-int testfunktion()
-{
-    printf("******** Funktionen-Test ********\n");
-
-    printf("\n****** Min-Test ******\n");
-    printf("Eingabe(x,y)\t| Ergebnis \n");
-    printf("----------------+----------\n");
-
-    int x = 15, y = 42;
-    int erg = min(x,y);
-    ausgabe(x,y,erg);
-
-    x = 1312;
-    y = 420;
-    erg = min(x,y);
-    ausgabe(x,y,erg);
-
-    x = 62;
-    y = x;
-    erg = min(x,y);
-    ausgabe(x,y,erg);
-
-
-    printf("\n****** Max-Test ******\n");
-    printf("Eingabe(x,y)\t| Ergebnis \n");
-    printf("----------------+----------\n");
-
-    x = 15, y = 42;
-    erg = max(x,y);
-    ausgabe(x,y,erg);
-
-    x = 1312;
-    y = 420;
-    erg = max(x,y);
-    ausgabe(x,y,erg);
-
-    x = 62;
-    y = x;
-    erg = max(x,y);
-    ausgabe(x,y,erg);
-
-
-    printf("\n****** Vorzeichen-Test ******\n");
-    printf("Eingabe(x)\t| Ergebnis \n");
-    printf("----------------+----------\n");
-
-    x = 96;
-    erg = vorzeichen(x);
-    ausgabe(x,NULL,erg);
-
-    x = -1337;
-    erg = vorzeichen(x);
-    ausgabe(x,NULL,erg);
-
-    x = 0;
-    erg = vorzeichen(x);
-    ausgabe(x,NULL,erg);
-
-
-    return 0;
-}
-
 int main()
 {
     int daysOfMonth, dayOfYear;
+
+    //Leeres Datum erzeugen
     Date date;
 
     printf("**** Test der neuen Funktionen ****\n");
 
+    //Datum über CLI einlesen
     input_date(&date);
 
+    //Überprüfen ob das Datum existiert
     if(exists_date(date)) {
         printf("Das Datum %d.%d.%d existiert.\n", date.day, date.month, date.year);
     } else  {
         printf("Das Datum %d.%d.%d existiert nicht.\n", date.day, date.month, date.year);
     }
 
+    //Die Tage des Monats herausfinden
     daysOfMonth = get_days_for_month(date.month, date.year);
     printf("Der %d. Monat hat %d Tage.\n", date.month, daysOfMonth);
+
+    //Den Tag des Jahres herausfinden
     dayOfYear = day_of_the_year(date);
     printf("Der %d.%d.%d ist der %d. Tag des Jahres.\n", date.day, date.month, date.year, dayOfYear);
 
